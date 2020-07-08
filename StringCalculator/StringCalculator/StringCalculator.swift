760efe986d636641b1c8007a42e74579b8afba3d
Biatel <sonia.biatel@xpeppers.com>
2020-07-08 16:46:15 +0200

    extract function

--- StringCalculator/StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator/StringCalculator.swift
@@ -2,12 +2,16 @@ import Foundation
 
 class StringCalculator {
     
+    private func toInt(_ string: String) -> Int {
+        return Int(string) ?? 0
+    }
+    
     func add(numbers: String) -> Int {
         if numbers.contains(",") {
             let numbersArray = numbers.split(separator: ",")
-            return Int(numbersArray[0])! + Int(numbersArray[1])!
+            return toInt(String(numbersArray[0])) + toInt(String(numbersArray[1]))
         }
         
-        return Int(numbers) ?? 0
+        return toInt(numbers)
     }
 }
