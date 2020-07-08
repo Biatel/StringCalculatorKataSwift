583cb17cdbb7e969eba097fdf4f1c19381eb60f9
Biatel <sonia.biatel@xpeppers.com>
2020-07-08 16:50:13 +0200

    extract an overloading to int method

--- StringCalculator/StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator/StringCalculator.swift
@@ -5,11 +5,15 @@ class StringCalculator {
     private func toInt(_ string: String) -> Int {
         return Int(string) ?? 0
     }
+
+    private func toInt(_ string: Substring) -> Int {
+        return Int(string) ?? 0
+    }
     
     func add(numbers: String) -> Int {
         if numbers.contains(",") {
             let numbersArray = numbers.split(separator: ",")
-            return toInt(String(numbersArray[0])) + toInt(String(numbersArray[1]))
+            return toInt(numbersArray[0]) + toInt(numbersArray[1])
         }
         
         return toInt(numbers)
