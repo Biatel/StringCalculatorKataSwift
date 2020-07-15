4a155dba5f1ab098ae02ac576464b3acee398ec4
Pietro Di Bello <pierodibello@gmail.com>
2020-07-15 15:58:09 +0200

    move private method down

--- StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator.swift
@@ -2,20 +2,20 @@ import Foundation
 
 class StringCalculator {
     
-    private func toInt(_ string: Substring) -> Int {
-        return Int(string)!
-    }
-    
     func add(numbers: String) -> Int {
         guard !numbers.isEmpty else {
             return 0
         }
-        
+
         return numbers
             .replacingOccurrences(of: "\n", with: ",")
             .split(separator: ",")
             .reduce(0) { (accumulation: Int, each: Substring) -> Int in
                 return accumulation + toInt(each)
         }
+    }
+
+    private func toInt(_ string: Substring) -> Int {
+        return Int(string)!
     }
 }
