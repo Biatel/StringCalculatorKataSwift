76fa2f5d307a8be5c1477bba4f4e8919b2be92ae
Pietro Di Bello <pierodibello@gmail.com>
2020-07-15 17:24:05 +0200

    refactor

--- StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator.swift
@@ -1,41 +1,42 @@
 import Foundation
 
 class StringCalculator {
-    
+    static let DEFAULT_DELIMITER = ","
+    static let CUSTOM_DELIMITER_PREFIX = "//"
+
     func add(numbers: String) -> Int {
-        var numbersToAdd: String
-        var separator: String
-        
         guard !numbers.isEmpty else {
             return 0
         }
         
-        if hasCustomDelimiter(numbers) {
-            separator = extractSeparatorFrom(numbers)
-            numbersToAdd = extractNumbersToAddFrom(numbers)
-        } else {
-            numbersToAdd = numbers
-            separator = ","
-        }
-        
-        return numbersToAdd
+        let separator = separatorFrom(numbers)
+
+        return numersToAddFrom(numbers)
             .replacingOccurrences(of: "\n", with: separator)
             .split(separator: Character(separator))
             .reduce(0) { (accumulation: Int, each: Substring) -> Int in
                 return accumulation + toInt(each) }
     }
 
+    private func separatorFrom(_ numbers: String) -> String {
+        return hasCustomDelimiter(numbers) ? extractSeparatorFrom(numbers) : StringCalculator.DEFAULT_DELIMITER
+    }
+    
+    private func numersToAddFrom(_ numbers: String) -> String {
+        return hasCustomDelimiter(numbers) ? extractNumbersToAddFrom(numbers) : numbers
+    }
+    
     private func customDelimiterAndNumbersToAddFrom(_ numbers: String) -> [String.SubSequence] {
         return numbers.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
     }
     
     private func hasCustomDelimiter(_ numbers: String) -> Bool {
-        return numbers.starts(with: "//")
+        return numbers.starts(with: StringCalculator.CUSTOM_DELIMITER_PREFIX)
     }
 
     private func extractSeparatorFrom(_ numbers: String) -> String {
         let customDelimiterAndNumbersToAdd = customDelimiterAndNumbersToAddFrom(numbers)
-        return String((customDelimiterAndNumbersToAdd.first?.dropFirst(2))!)
+        return String((customDelimiterAndNumbersToAdd.first?.dropFirst(StringCalculator.CUSTOM_DELIMITER_PREFIX.count))!)
     }
     
     private func extractNumbersToAddFrom(_ numbers: String) -> String {
