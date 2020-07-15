82c20d74256fdecb05c6504347be44447340cf81
Pietro Di Bello <pierodibello@gmail.com>
2020-07-15 17:13:28 +0200

    refactor

--- StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator.swift
@@ -3,17 +3,19 @@ import Foundation
 class StringCalculator {
     
     func add(numbers: String) -> Int {
-        var numbersToAdd: String = numbers
-        var separator: String = ","
+        var numbersToAdd: String
+        var separator: String
         
         guard !numbers.isEmpty else {
             return 0
         }
         
-        if numbers.starts(with: "//") {
-            let x = numbers.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
-            separator = String((x.first?.dropFirst(2))!)
-            numbersToAdd = String(x.last!)
+        if hasCustomDelimiter(numbers) {
+            separator = extractSeparatorFrom(numbers)
+            numbersToAdd = extractNumbersToAddFrom(numbers)
+        } else {
+            numbersToAdd = numbers
+            separator = ","
         }
         
         return numbersToAdd
@@ -21,6 +23,24 @@ class StringCalculator {
             .split(separator: Character(separator))
             .reduce(0) { (accumulation: Int, each: Substring) -> Int in
                 return accumulation + toInt(each) }
+    }
+
+    private func customDelimiterAndNumbersToAddFrom(_ numbers: String) -> [String.SubSequence] {
+        return numbers.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
+    }
+    
+    private func hasCustomDelimiter(_ numbers: String) -> Bool {
+        return numbers.starts(with: "//")
+    }
+
+    private func extractSeparatorFrom(_ numbers: String) -> String {
+        let customDelimiterAndNumbersToAdd = customDelimiterAndNumbersToAddFrom(numbers)
+        return String((customDelimiterAndNumbersToAdd.first?.dropFirst(2))!)
+    }
+    
+    private func extractNumbersToAddFrom(_ numbers: String) -> String {
+        let customDelimiterAndNumbersToAdd = customDelimiterAndNumbersToAddFrom(numbers)
+        return String(customDelimiterAndNumbersToAdd.last!)
     }
     
     private func toInt(_ string: Substring) -> Int {
